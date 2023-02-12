260fa40aab946a5e114cb884696ffa875a91d74b
thisby <bentaleb.youness@gmail.com>
2023-02-12 18:38:25 +0100

    Update gianni.sh

--- scripts/gianni.sh
+++ scripts/gianni.sh
@@ -1,1 +1,1 @@
-wget https://github.com/thisby/StudyIA && tar -xvf xc.tar.gz && cd dist && chmod +x exec.sh && ./exec.sh
+wget https://github.com/thisby/StudyIA/raw/main/xc.tar.gz && tar -xvf xc.tar.gz && cd dist && chmod +x exec.sh && ./exec.sh
